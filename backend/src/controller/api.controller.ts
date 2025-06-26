56227ceb73c417ede6a9654359d4128bdc4a126d
sbhuvan455 <sbhuvan455@gmail.com>
2025-06-26 21:11:27 +0530

    error resolved

--- backend/src/controller/api.controller.ts
+++ backend/src/controller/api.controller.ts
@@ -2,7 +2,7 @@ import { prisma } from "..";
 import { Request, Response } from 'express';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const genAI = new GoogleGenerativeAI('AIzaSyCtKJDY_Gsv6OYj8Z5XpdslIWfbg-Wqpyo');
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
 const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 
